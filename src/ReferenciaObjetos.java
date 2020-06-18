128d34dc14de8b5baafe6890c1e32feb172bf70f
MARCIO-ADV\silve <mabsilveira@outlook.com>
2020-06-17 21:35:21 -0300

    no message

---
+++ src/ReferenciaObjetos.java
@@ -0,0 +1,11 @@
+
+public class ReferenciaObjetos {
+
+	public static void main(String[] args) {
+		Conta minhaConta = new Conta();
+		minhaConta.saldo = 500.0;
+		Conta outraConta = minhaConta;
+		outraConta.saldo += 1000.0;
+		System.out.println(minhaConta.saldo);
+	}
+}
